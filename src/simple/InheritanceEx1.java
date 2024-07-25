c1a1feba1db686a83b833a0bd3835c120b47ba7c
SumitGit27 <rdj05424@gmail.com>
2024-07-25 18:23:58 +0530

    changes

---
+++ src/simple/InheritanceEx1.java
@@ -0,0 +1,18 @@
+package simple;
+
+class parent{
+	public void demo() {
+		System.out.println("This is Demo method");
+	}
+}
+
+public class InheritanceEx1 extends parent {
+	public void demo() {
+		System.out.println("This is Demo method");
+	}
+	public static void main(String[] args) {
+		InheritanceEx1 obj = new InheritanceEx1();
+		obj.demo();
+	}
+
+}

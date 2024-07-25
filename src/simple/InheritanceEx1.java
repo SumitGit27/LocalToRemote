a39407b0567c5e76e3d50835c8ace19f460f23df
SumitGit27 <rdj05424@gmail.com>
2024-07-25 19:31:41 +0530

    ...

--- src/simple/InheritanceEx1.java
+++ src/simple/InheritanceEx1.java
@@ -8,7 +8,7 @@ class parent{
 
 public class InheritanceEx1 extends parent {
 	public void demo() {
-		System.out.println("This is Demo method");
+		System.out.println("This is Demo method is overloaded");
 	}
 	public static void main(String[] args) {
 		InheritanceEx1 obj = new InheritanceEx1();
